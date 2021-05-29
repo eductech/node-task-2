cc0a4f46484369e759ad9024e3e6b87eac8bd8d2
alex.zamirouski <alex.zamirouski@gmail.com>
2021-05-29 19:49:44 +0300

    doc: jsdoc docs added

--- src/resources/boards/boards.model.js
+++ src/resources/boards/boards.model.js
@@ -1,6 +1,21 @@
 const { v4: uuid } = require('uuid');
 
+/**
+ * @typedef {Object} Column
+ * @property {string} title` - column title.
+ * @property {number} order - column order.
+ */
+
+/**
+ * Class representing a board.
+ */
 class Board {
+  /**
+   * Create a board.
+   * @param {string} id - unique instance id.
+   * @param {string} title - board title.
+   * @param {Column[]} columns - board columns.
+   */
   constructor({
     id = uuid(),
     title = 'TITLE',
@@ -11,18 +26,31 @@ class Board {
     this.columns = columns;
   }
 
+  /**
+   * Convert request payload to board object.
+   * @param {object} data - Object payload that represents task input from client.
+   * @return {Board} A Board object.
+   */
   static fromRequest(data) {
     return new Board(data);
   }
 
+  /**
+   * Convert board instance to object that could be used as response payload.
+   * @param {Board} board - Board instance.
+   * @return {Board} board info without sensitive data.
+   */
   static toResponse(board) {
-    const { id, title, columns } = board;
-    return { id, title, columns };
+    return board;
   }
 
+  /**
+   * Convert board instance to object that could be stored in db.
+   * @param {Board} task - Board instance.
+   * @return {Board} Board instance.
+   */
   static toDb(board) {
-    const { id, title, columns } = board;
-    return { id, title, columns };
+    return board;
   }
 }
 
